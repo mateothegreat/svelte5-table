de32ab3183c178902dc4f499831e89c3a7bdc324
Matthew Davis <matthew@matthewdavis.io>
2024-10-24 01:06:14 -0500

    bump

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 export { default as Body } from "./manual/table-body.svelte";
 export { default as Head } from "./manual/table-head.svelte";
 export { default as Header } from "./manual/table-header.svelte";
-export type { TableColumn } from "./types";
 
 export * from "./dropin";
+export * from "./types";
+
